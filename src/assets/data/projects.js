480557be994fa4dd52ac6171927b6942b0843db3
harsh599599 <harshthakkar599@gmail.com>
2021-08-10 20:21:17 +0530

    add blog proj

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import ProjectImg from '../images/projectImg.png';
 import UTrackerImg from '../images/utracker.jpg';
+import BlogImg from '../images/Capture blog.png';
 import GreenCtgImg from '../images/greenctg.jpg';
 import CoinTrackerImg from '../images/cointracker.jpg';
 import CavinImg from '../images/cavinimg.jpg';
@@ -16,11 +17,11 @@ const projects = [
   },
   {
     id: uuidv4(),
-    name: 'E-Commerce',
+    name: 'Create of Blog',
     desc:
-      'An application to manage items,categories from Admin Dashboard and user can see and place the order I have added Stripe and paypal payment gateway with testing key.',
-    img: UTrackerImg,
-    Links: 'https://github.com/harsh599599/prac-projects',
+      'An application in which you can write your blog,you can update and delete blog.',
+    img: BlogImg,
+    Links: 'https://createyourblog-harshthakkar.netlify.app',
   },
   {
     id: uuidv4(),
